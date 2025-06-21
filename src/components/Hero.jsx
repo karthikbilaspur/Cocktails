5536feb123afd5533e73b40f1207ac35333d52cf
[karthikbilaspur] <[karthikv81291@gmail.com]>
2025-06-21 14:43:59 +0530

    Made a Cocktail website using GSAP, Tailwind CSS, ReactJs

---
+++ src/components/Hero.jsx
@@ -0,0 +1,135 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
+
+/**
+ * Hero component that displays a animated title and subtitle.
+ */
+const Hero = () => {
+  const videoRef = useRef(null);
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useGSAP(() => {
+    const animateTitle = () => {
+      const titleSplit = new SplitText(titleRef.current, {
+        type: "chars, words",
+      });
+
+      gsap.from(titleSplit.chars, {
+        yPercent: 100,
+        duration: 1.8,
+        ease: "expo.out",
+        stagger: 0.06,
+      });
+    };
+
+    const animateSubtitle = () => {
+      const subtitleSplit = new SplitText(subtitleRef.current, {
+        type: "lines",
+      });
+
+      gsap.from(subtitleSplit.lines, {
+        opacity: 0,
+        yPercent: 100,
+        duration: 1.8,
+        ease: "expo.out",
+        stagger: 0.06,
+        delay: 1,
+      });
+    };
+
+    animateTitle();
+    animateSubtitle();
+
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#hero",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+
+    parallaxTimeline
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const videoTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      videoTimeline.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
+  }, []);
+
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 ref={titleRef} className="title">
+          MOJITO
+        </h1>
+
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p ref={subtitleRef} className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+          src="/videos/output.mp4"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Hero;
